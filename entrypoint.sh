e63a04208fe1e1ad56fd5444d2fc1c2b23c67e23
ez1zena <argaz.sl@gmail.com>
2025-04-15 23:56:47 +0200

    Update and rename docker-compose.yml to entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Parsear DATABASE_URL si existe
+if [[ -n "$DATABASE_URL" ]]; then
+  export DB_HOST=$(echo $DATABASE_URL | sed -E 's|.*@([^:/]+).*|\1|')
+  export DB_PORT=$(echo $DATABASE_URL | sed -E 's|.*:([0-9]+)/.*|\1|')
+  export DB_USER=$(echo $DATABASE_URL | sed -E 's|postgresql://([^:]+):.*|\1|')
+  export DB_PASSWORD=$(echo $DATABASE_URL | sed -E 's|postgresql://[^:]+:([^@]+)@.*|\1|')
+  export DB_NAME=$(echo $DATABASE_URL | sed -E 's|.*/([^/?]+).*|\1|')
+fi
+
+# Ejecutar Odoo con los valores extraídos
+exec odoo \
+  --db_host=$DB_HOST \
+  --db_port=${DB_PORT:-5432} \
+  --db_user=$DB_USER \
+  --db_password=$DB_PASSWORD \
+  --db_name=$DB_NAME
