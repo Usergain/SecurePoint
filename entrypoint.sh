5001bd56df38bf92c5ec2917ebea75a99099a3af
ez1zena <argaz.sl@gmail.com>
2025-04-16 09:15:30 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,25 @@
-#!/bin/bash
+#!/bin/sh
 
-echo "🚀 Inicializando la base de datos si no existe..."
+set -e
 
-exec odoo -i base
+echo Waiting for database...
 
-echo "✅ Base de datos lista. Lanzando Odoo..."
+while ! nc -z ${ODOO_DATABASE_HOST} ${ODOO_DATABASE_PORT} 2>&1; do sleep 1; done; 
+
+echo Database is now available
+
+exec odoo \
+    --http-port="${PORT}" \
+    --init=all \
+    --without-demo=True \
+    --proxy-mode \
+    --db_host="${ODOO_DATABASE_HOST}" \
+    --db_port="${ODOO_DATABASE_PORT}" \
+    --db_user="${ODOO_DATABASE_USER}" \
+    --db_password="${ODOO_DATABASE_PASSWORD}" \
+    --database="${ODOO_DATABASE_NAME}" \
+    --smtp="${ODOO_SMTP_HOST}" \
+    --smtp-port="${ODOO_SMTP_PORT_NUMBER}" \
+    --smtp-user="${ODOO_SMTP_USER}" \
+    --smtp-password="${ODOO_SMTP_PASSWORD}" \
+    --email-from="${ODOO_EMAIL_FROM}" 2>&1
