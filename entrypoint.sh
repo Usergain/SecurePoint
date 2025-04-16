22a1dcd4d789d4d280cd003f75555fd11d614dfe
ez1zena <argaz.sl@gmail.com>
2025-04-16 09:00:05 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,6 @@
 
 echo "🚀 Inicializando la base de datos si no existe..."
 
-exec odoo \
-  --db_host="$DB_HOST" \
-  --db_port="${DB_PORT:-5432}" \
-  --db_user="$DB_USER" \
-  --db_password="$DB_PASSWORD" \
-  -d "$DB_NAME"
-  -i base
+exec odoo -i base
 
-  echo "✅ Base de datos lista. Lanzando Odoo..."
+echo "✅ Base de datos lista. Lanzando Odoo..."
