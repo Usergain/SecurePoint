db1f0c0f2d9c462cb4a652b11d08d7813030c07a
ez1zena <argaz.sl@gmail.com>
2025-04-16 09:30:34 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,24 +2,20 @@
 
 set -e
 
-echo Waiting for database...
+echo "⏳ Esperando a la base de datos..."
 
-while ! nc -z ${ODOO_DATABASE_HOST} ${ODOO_DATABASE_PORT} 2>&1; do sleep 1; done; 
+# Esperar a que la base de datos esté disponible
+while ! nc -z ${DB_HOST} ${DB_PORT} 2>&1; do sleep 1; done; 
 
-echo Database is now available
+echo "✅ Base de datos disponible. Iniciando Odoo..."
 
 exec odoo \
     --http-port="${PORT}" \
-    --init=all \
+    --init=base \
     --without-demo=True \
     --proxy-mode \
-    --db_host="${ODOO_DATABASE_HOST}" \
-    --db_port="${ODOO_DATABASE_PORT}" \
-    --db_user="${ODOO_DATABASE_USER}" \
-    --db_password="${ODOO_DATABASE_PASSWORD}" \
-    --database="${ODOO_DATABASE_NAME}" \
-    --smtp="${ODOO_SMTP_HOST}" \
-    --smtp-port="${ODOO_SMTP_PORT_NUMBER}" \
-    --smtp-user="${ODOO_SMTP_USER}" \
-    --smtp-password="${ODOO_SMTP_PASSWORD}" \
-    --email-from="${ODOO_EMAIL_FROM}" 2>&1
+    --db_host="${DB_HOST}" \
+    --db_port="${DB_PORT}" \
+    --db_user="${DB_USER}" \
+    --db_password="${DB_PASSWORD}" \
+    --database="${DB_NAME}" 2>&1
