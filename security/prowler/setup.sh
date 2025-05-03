b24ede499636a02288325f017da2b919ee9a106d
ez1zena <argaz.sl@gmail.com>
2025-05-03 18:24:02 +0200

    Update setup.sh

--- security/prowler/setup.sh
+++ security/prowler/setup.sh
@@ -1,1 +1,5 @@
-
+#!/bin/bash
+# Instalar Prowler (no requiere Docker)
+sudo apt-get install -y python3 python3-pip
+pip3 install prowler-cloud
+echo "Ejecutar auditoría con: prowler -c check13 (ejemplo)"
